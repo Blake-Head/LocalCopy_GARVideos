5df6f8412026acf7597b91256353663f4ad93368
Antonio Cascio <acascio@leomail.tamuc.edu>
2024-10-29 11:00:26 -0500

    fixed the manimlib import so the code works in VSCode now

--- Antonio_Projects/ManimGL Tests/Transformation_test.py
+++ Antonio_Projects/ManimGL Tests/Transformation_test.py
@@ -1,5 +1,6 @@
 from manim import *
-from manimlib import *
+from manimlib.imports import *
+    # for all future code, use this import
 
 class SquareToCircle(Scene):
     def construct(self):
@@ -12,7 +13,7 @@ class SquareToCircle(Scene):
         circle.set_fill(BLUE, opacity=0.5)
 
         # Adding the shapes to the scene
-        self.play(Create(square))
+        self.play(ShowCreation(square))
         self.wait(1)
 
         # Transformation time
