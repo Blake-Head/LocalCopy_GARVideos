1ff1e6a3c49cfc552befb08ef12eb4fa896bde21
Antonio Cascio <acascio@leomail.tamuc.edu>
2024-11-05 12:13:25 -0600

    Coding genius friend helped me try to understand what is wrong but nothing we did worked. Therefore, I am crying

--- Antonio_Projects/ManimGL Tests/e_field_test.py
+++ Antonio_Projects/ManimGL Tests/e_field_test.py
@@ -1,15 +1,16 @@
 from manim import *
-from manim_physics import *
+# from manim_physics import *
+import manim_physics as mp
 import numpy as np
 
 # Making a charged particle influence an electric field
 class PointChargeInElectricField(Scene):
     def construct(self):
         # Create the electric field
-        field = ElectricField()
+        field = mp.ElectricField()
         
         # Create the point charge
-        pointCharge = Charge(magnitude=1, position=np.array([-3, 0, 0]))
+        pointCharge = mp.Charge(magnitude=1, position=np.array([-3, 0, 0]))
 
         # Add the electric field and the point charge to the scene
         self.add(field, pointCharge)
@@ -22,30 +23,30 @@ class PointChargeInElectricField(Scene):
 
         self.wait(1)
 
-class PointChargeInVectorField(Scene):
-    def construct(self):
-        # Create electric field
-        vector_field = ArrowVectorField(
-            lambda pos: np.array([pos[0], pos[1], 0]),  # simple linear field for visualization
-            x_range=[-5, 5, 1],
-            y_range=[-3, 3, 1]
-        )
+# class PointChargeInVectorField(Scene):
+#     def construct(self):
+#         # Create electric field
+#         vector_field = ArrowVectorField(
+#             lambda pos: np.array([pos[0], pos[1], 0]),  # simple linear field for visualization
+#             x_range=[-5, 5, 1],
+#             y_range=[-3, 3, 1]
+#         )
         
-        # Create a point charge
-        point_charge = Dot(color=RED).move_to(np.array([-3, 0, 0]))
-        charge_label = Tex("+Q").next_to(point_charge, UP, buff=0.1)
+#         # Create a point charge
+#         point_charge = Dot(color=RED).move_to(np.array([-3, 0, 0]))
+#         charge_label = Tex("+Q").next_to(point_charge, UP, buff=0.1)
 
-        # Add the vector field and point charge to the scene
-        self.add(vector_field, point_charge, charge_label)
+#         # Add the vector field and point charge to the scene
+#         self.add(vector_field, point_charge, charge_label)
         
-        # Define the movement path for the point charge
-        path = ParametricFunction(
-            lambda t: np.array([-3 + 2*t, np.sin(t*PI), 0]),  # Path from left to right with a sine wave motion
-            t_range=np.array([0, 3, 0.01]),
-            color=RED
-        )
+#         # Define the movement path for the point charge
+#         path = ParametricFunction(
+#             lambda t: np.array([-3 + 2*t, np.sin(t*PI), 0]),  # Path from left to right with a sine wave motion
+#             t_range=np.array([0, 3, 0.01]),
+#             color=RED
+#         )
 
-        # Create the animation of the point charge moving along the path
-        self.play(MoveAlongPath(point_charge, path), run_time=5, rate_func=linear)
+#         # Create the animation of the point charge moving along the path
+#         self.play(MoveAlongPath(point_charge, path), run_time=5, rate_func=linear)
 
-        self.wait(2)
+#         self.wait(2)
--- Antonio_Projects/whyIsNothingWorking.py
+++ Antonio_Projects/whyIsNothingWorking.py
@@ -1,6 +1,6 @@
 from manim import *
 from manimlib.imports import *
-from manim_physics import *
+#from manim_physics import *
 #import numpy as np
 
 # Code to test why literally all of the previous made scene do not run
@@ -11,7 +11,7 @@ class ImSad(Scene):
 
         self.add(circle)
         self.play(Create(circle))
-        #self.play(ShowCreation(circle))
+        self.play(ShowCreation(circle))
         self.wait(1)
 
 # Code produces a recursion error
@@ -19,19 +19,37 @@ class ImSad(Scene):
 # Now says no Scenes in module
 
 # Rewrite Circle to Sqaure
-class CircleToSquare(Scene):
-    def construct(self):
-        circle = Circle()
-        circle.set_fill(RED, opacity=0.5)
+# class CircleToSquare(Scene):
+#     def construct(self):
+#         circle = Circle()
+#         circle.set_fill(RED, opacity=0.5)
 
-        square = Square()
-        square.set_fill(BLUE, opacity=0.5)
+#         square = Square()
+#         square.set_fill(BLUE, opacity=0.5)
 
-        self.play(ShowCreation(circle))
-        self.wait(1)
+#         self.play(ShowCreation(circle))
+#         self.wait(1)
 
-        self.play(Transform(circle, square))
-        self.wait(1)
+#         self.play(Transform(circle, square))
+#         self.wait(1)
 
 # Literally the same code as the other transformation_test.py except swapped
 #   circle with sqaure and it does not run
+
+class SquareToCircle(Scene):
+    def construct(self):
+        # Create the square first
+        square = Square()
+        square.set_fill(RED, opacity=0.5)
+
+        # Create the circle
+        circle = Circle()
+        circle.set_fill(BLUE, opacity=0.5)
+
+        # Adding the shapes to the scene
+        self.play(ShowCreation(square))
+        self.wait(1)
+
+        # Transformation time
+        self.play(Transform(square, circle))
+        self.wait(1)
